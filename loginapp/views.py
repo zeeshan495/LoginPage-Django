c6770e8c0e7deed3b537ab62760f09858c72440b
vision <shaikzeeshan88@gmail.com>
2018-09-12 16:48:33 +0530

    sessions updated

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader, RequestContext
-from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth import authenticate, login, get_user_model, logout
 from .forms import LoginForm,RegisterForm
 from django.contrib.auth.forms import UserCreationForm
 
@@ -10,17 +10,23 @@ def index_page(request):
     if request.session.has_key('username'):
         username = request.session['username']
         return render(request, 'home.html', {"username": username})
-    context = {"tittle": "hello... it is home page"
-               }
-    return render(request,"index.html",context)
+    else:
+        context = {"tittle": "hello... it is home page"
+                   }
+        return render(request,"index.html",context)
 
 
 def logout_page(request):
     try:
+        logout(request)
         del request.session['username']
     except:
         pass
-    return redirect("http://127.0.0.1:8000/login/")
+    form = LoginForm(request.POST or None)
+    context = {"form": form}
+    return render(request, "login.html", context)
+
+    # return redirect("http://127.0.0.1:8000/login/")
 
 
 def home_page(request):
@@ -30,7 +36,10 @@ def home_page(request):
                    "username": username
                    }
         return render(request, 'home.html', context)
-
+    else:
+        form = LoginForm(request.POST or None)
+        context = {"form": form}
+        return render(request, "login.html", context)
     # if request.user.is_authenticated():
     #     context["premium"] = "you already logged in"
     # form = LoginForm(request.POST or None)
@@ -39,30 +48,31 @@ def home_page(request):
 
 
 def login_page(request):
-    # if request.session.has_key('username'):
-    #     username = request.session['username']
-    #     return render(request, 'home.html', {"username": username})
-    form = LoginForm(request.POST or None)
-    context = {"form": form}
-    # print(request.user.is_authenticated())
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=username, password=password)
-            request.session['username'] = username
-            print(user)
-            if user is not None:
-                login(request, user)
-                # Redirect to a success page.
-                # context['form'] = LoginForm()
-                return redirect("home")
-            else:
-                # Return an 'invalid login' error message.
-                print("Error")
-    return render(request, "login.html", context)
+    if request.session.has_key('username'):
+        username = request.session['username']
+        return render(request, 'home.html', {"username": username})
+    else:
+        form = LoginForm(request.POST or None)
+        context = {"form": form}
+        # print(request.user.is_authenticated())
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                print(form.cleaned_data)
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+                user = authenticate(request, username=username, password=password)
+                request.session['username'] = username
+                print(user)
+                if user is not None:
+                    login(request, user)
+                    # Redirect to a success page.
+                    # context['form'] = LoginForm()
+                    return redirect("home")
+                else:
+                    # Return an 'invalid login' error message.
+                    print("Error")
+        return render(request, "login.html", context)
 
 User = get_user_model()
 def register_page(request):
